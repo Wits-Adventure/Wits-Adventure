db89c5e363cf5e01de013ee0c24f39354c7de680
TafaraC <tafarachibanda@outlook.com>
2025-08-12 14:43:41 +0200

    merged from auth features and resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/homepage';
 import Login from './components/login';
 import Signup from './components/signup';
