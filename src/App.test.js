065754ce7242dd31b9c94fda096cac0a5a81fee8
tetelochuene <tetelolchuene@gmail.com>
2025-08-14 15:27:26 +0200

    Fixed file structure and added ProfilePage

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,13 @@
+// App.test.js
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+// Mock react-router-dom so Jest can run
+jest.mock('react-router-dom', () => ({
+  BrowserRouter: ({ children }) => <div>{children}</div>,
+  Routes: ({ children }) => <div>{children}</div>,
+  Route: ({ element }) => <div>{element}</div>
+}));
 
 test('renders welcome message', () => {
   render(<App />);
